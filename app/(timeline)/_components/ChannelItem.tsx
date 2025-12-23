5409b0002658390a6ca799d3b9d9175f4bd401f0
lyq <nanhualyq@gmail.com>
2025-12-23 11:04:23 +0800

    factor: channel item UI

--- app/(timeline)/_components/ChannelItem.tsx
+++ app/(timeline)/_components/ChannelItem.tsx
@@ -1,13 +1,5 @@
 "use client";
 import { channelTable } from "@/src/db/schema";
-import {
-  DeleteOutlined,
-  EditOutlined,
-  EllipsisOutlined,
-  RestOutlined,
-  SyncOutlined,
-} from "@ant-design/icons";
-import { Dropdown, MenuProps, Modal } from "antd";
 import styles from "./ChannelItem.module.css";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useRef } from "react";
@@ -16,45 +8,34 @@ import {
   deleteArticlesByChannel,
   deleteChannel,
 } from "@/app/actions";
-import useMessage from "antd/es/message/useMessage";
 import ChannelTitle from "./ChannelTitle";
 import CountShow from "./CountShow";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  IconDots,
+  IconEdit,
+  IconRefresh,
+  IconTrashX,
+  IconWashDryOff,
+} from "@tabler/icons-react";
+import { toast } from "sonner";
+import Swal from "sweetalert2";
 
 interface Props {
   channel: typeof channelTable.$inferSelect;
 }
-
-const menuItems: MenuProps["items"] = [
-  {
-    key: "Edit",
-    label: "Edit",
-    icon: <EditOutlined />,
-  },
-  {
-    key: "Refresh",
-    label: "Refresh",
-    icon: <SyncOutlined />,
-  },
-  {
-    key: "Delete",
-    label: "Delete",
-    danger: true,
-    icon: <DeleteOutlined />,
-  },
-  {
-    key: "Empty",
-    label: "Empty",
-    danger: true,
-    icon: <RestOutlined />,
-  },
-];
 
 export default function ChannelItem({ channel }: Props) {
   const sp = useSearchParams();
   const isFiltering = sp.get("channel") === channel.id + "";
   const rootRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
-  const [messageApi, contextHolder] = useMessage();
 
   useEffect(() => {
     if (isFiltering) {
@@ -65,73 +46,84 @@ export default function ChannelItem({ channel }: Props) {
     }
   }, [isFiltering]);
 
-  function handleAction(cb: () => Promise<unknown>, target = "/") {
-    messageApi.open({
-      type: "loading",
-      content: "Action in progress..",
-      duration: 0,
-    });
-    cb()
-      .then(() => {
-        router.replace(target);
-        location.reload();
-      })
-      .catch((error) => {
-        const modal = Modal.error({
-          content: error + "",
-          onOk() {
-            modal.destroy();
-          },
-        });
-      })
-      .finally(() => {
-        messageApi.destroy();
-      });
-  }
-
-  function handleDelete(action: string) {
+  async function handleDelete(action: string) {
     const isDelete = action === "Delete";
     const title = isDelete
       ? "Delete this channel?"
       : "Delete all articles of this channel";
-    Modal.confirm({
+    await Swal.fire({
       title,
-      onOk() {
-        handleAction(() => {
-          return (isDelete ? deleteChannel : deleteArticlesByChannel)(
-            channel.id
-          );
-        });
-      },
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: action,
+      cancelButtonText: "Cancel",
     });
+    toast.promise(
+      (isDelete ? deleteChannel : deleteArticlesByChannel)(channel.id),
+      {
+        loading: `${action} is in progress...`,
+        success: () => {
+          router.replace("/");
+          location.reload();
+          return "Successed!";
+        },
+        error: "Error",
+      }
+    );
   }
 
-  const onMenuClick: MenuProps["onClick"] = ({ key }) => {
-    if (key === "Edit") {
-      router.push(`/channel/${channel.id}/edit`);
-    } else if (["Delete", "Empty"].includes(key)) {
-      handleDelete(key);
-    } else if (key === "Refresh") {
-      handleAction(() => channelCrawler(channel), `?channel=${channel.id}`);
-    }
-  };
+  function handleRefresh() {
+    toast.promise(channelCrawler(channel), {
+      loading: "Channel is refresh...",
+      success: () => {
+        router.replace(`?channel=${channel.id}`);
+        location.reload();
+        return "Successed!";
+      },
+      error: "Error",
+    });
+  }
 
   return (
     <div
       ref={rootRef}
       className={`${styles.root} ${isFiltering ? styles.active : ""}`}
     >
-      {contextHolder}
       <ChannelTitle channel={channel} style={{ flex: 1 }} />
       <CountShow channel={channel.id} />
-      <Dropdown
-        menu={{ items: menuItems, onClick: onMenuClick }}
-        trigger={["click"]}
-      >
-        <a onClick={(e) => e.preventDefault()}>
-          <EllipsisOutlined style={{ padding: ".5rem" }} />
-        </a>
-      </Dropdown>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <IconDots />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-16" align="start">
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              onClick={() => router.push(`/channel/${channel.id}/edit`)}
+            >
+              <IconEdit />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={handleRefresh}>
+              <IconRefresh />
+              Refresh
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-red-500"
+              onClick={() => handleDelete("Delete")}
+            >
+              <IconTrashX />
+              Delete
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-red-500"
+              onClick={() => handleDelete("Empty")}
+            >
+              <IconWashDryOff />
+              Empty
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
